61f4aae0b4cb2b024fe1f4155570b1ecd74eb14b
Jingo88 <jcory.ng@gmail.com>
2015-01-18 10:54:55 -0500

    building a movie app

---
+++ movie.js
@@ -0,0 +1,32 @@
+var submit = document.querySelector('button');
+var input = document.querySelector('input');
+var img = document.querySelector('img');
+var title = document.querySelector('#title');
+
+//run the find poster function on click or enter
+submit.addEventListener('click', function() {
+    findPoster();
+});
+input.addEventListener('keyup', function(e) {
+    if (e.keyCode === 13) {
+        findPoster();
+    }
+})
+
+function findPoster() {
+    var movie = input.value
+    var movie_url = encodeURI(movie)
+    var url = "http://omdbapi.com/?t=" + movie_url
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', function(e) {
+        var d = xhr.responseText
+        var parsed = JSON.parse(d)
+        
+        var title = document.querySelector('#title');
+
+        img.src = parsed.Poster;
+        title = parsed.name;
+    })
+    xhr.open("GET", url);
+    xhr.send();
+}
