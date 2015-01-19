d4026ee62a671d76697060eabc1c1c87ce2912ca
Jingo88 <jcory.ng@gmail.com>
2015-01-19 18:15:01 -0500

    added RT api and example, going to implement them now

--- movie.js
+++ movie.js
@@ -128,6 +128,7 @@ $('.movieRank').click(function(){
         listmovieInfo.appendChild(listwriters);
         listmovieInfo.appendChild(listgenre);
 
+        console.log(parsed)
     })
     xhr.open("GET", url);
     xhr.send();
@@ -192,3 +193,39 @@ function findPoster() {
     xhr.open("GET", url);
     xhr.send();
 }
+
+//THIS IS YOUR RT API KEY
+// ten5jh5qwb6zsq3c6z5gwhrm
+
+//Rotten Tomatoes API Example with Javascript
+
+// var apikey = "myapikey";
+// var baseUrl = "http://api.rottentomatoes.com/api/public/v1.0";
+ 
+// // construct the uri with our apikey
+// var moviesSearchUrl = baseUrl + '/movies.json?apikey=' + apikey;
+// var query = "Gone with the Wind";
+ 
+// $(document).ready(function() {
+ 
+//   // send off the query
+//   $.ajax({
+//     url: moviesSearchUrl + '&q=' + encodeURI(query),
+//     dataType: "jsonp",
+//     success: searchCallback
+//   });
+// });
+ 
+// // callback for when we get back the results
+// function searchCallback(data) {
+//  $(document.body).append('Found ' + data.total + ' results for ' + query);
+//  var movies = data.movies;
+//  $.each(movies, function(index, movie) {
+//    $(document.body).append('<h1>' + movie.title + '</h1>');
+
+//    $(document.body).append('<img src="' + movie.posters.thumbnail + '" />');
+
+//  });
+
+// }
+
